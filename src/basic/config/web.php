06f2c59e999d6b062092806fd8077a0ed4cb4a3f
lucasfratini <matias_anubis@hotmail.com>
2022-04-03 16:14:05 -0300

    arrancando con gii

--- src/basic/config/web.php
+++ src/basic/config/web.php
@@ -43,14 +43,13 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
+
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
-            'rules' => [
-            ],
+            'rules' => [],
         ],
-        */
+
     ],
     'params' => $params,
 ];
@@ -68,7 +67,8 @@ if (YII_ENV_DEV) {
     $config['modules']['gii'] = [
         'class' => 'yii\gii\Module',
         // uncomment the following to add your IP if you are not connecting from localhost.
-        //'allowedIPs' => ['127.0.0.1', '::1'],
+        // 'allowedIPs' => ['127.0.0.1', '::1'],
+        'allowedIPs' => [($_ENV["PRODUCCION"]) ? '127.0.0.1' : '*'],
     ];
 }
 
